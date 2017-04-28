9ba4291af5f409acfa4f3e368654fd376168ec39
Vaughan Knight <vaughan@vaughanknight.com>
2017-04-28 12:14:45 +1000

    Unity Core libraries
    Fist commit, includes the package.  Package shoudl always be rexported
    if commiting any of the core scripts to make it nice and easy to update
    and pull into projects.

---
+++ UnityCoreProject/Assets/Scripts/Core/SingletonBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The SingletonBehaviour ensures that
+/// there is only ever a single copy of an inheriting behaviour, that in turn allows Singleton like
+/// interactions i.e Singleton.Instance.  It will dstroy any GameObject with subsequent behaviours of  
+/// the same type.  Note this is not just deleting the singleton behaviour to stop GameObject leaks.
+/// 
+/// It will also log an error when a duplicate Singleton Behaviour is created 
+/// so you can clean up your project.
+/// 
+/// Inherits from SmartBehaviour because everything should, even singletons.
+/// </summary>
+public class SingletonBehaviour<T> : SmartBehaviour where T : SmartBehaviour
+{
+    private const string TEXT_SINGLETON_EXISTS_ERROR = "Singleton '{0}' is already created. Destroying the object.";
+    private static SmartBehaviour _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            return (T)_instance;
+        }
+    }
+
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            // This is the first instance of the behaviour
+            _instance = this;
+        }
+        else
+        {
+            // Log an error so developers can clean up their code
+            Debug.LogFormat(TEXT_SINGLETON_EXISTS_ERROR, this.GetType().Name);
+
+            // Destroy the entire game object this behaviour is attached to
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+}
---
+++ UnityCoreProject/Assets/Scripts/Core/SmartBehaviour.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Behaviour that wraps all the bits that have heavy
+/// performace overhead with common 'getComponent' calls.
+/// 
+/// Why 'Smart'behaviour?  Because seriously.
+/// 
+/// NOTE: Feel free to add common accessors, generally which relate
+/// to those that have been deprecated for .GetComponent<T> which assumed
+/// a single instance i.e transform, renderer, rigidbody etc
+/// </summary>
+public class SmartBehaviour : MonoBehaviour {
+
+    private Transform _transform;
+
+    public Transform _Transform
+    {
+        get
+        {
+            if(_transform == null)
+            {
+                _transform = this.transform;
+            }
+            return _transform;
+        }
+    }
+
+    private Renderer _renderer;
+
+    public Renderer _Renderer
+    {
+        get
+        {
+            if (_renderer == null)
+            {
+                _renderer = this.GetComponent<Renderer>();
+            }
+            return _renderer;
+        }
+    }
+
+    private Rigidbody _rigidBody;
+
+    public Rigidbody _RigidBody
+    {
+        get
+        {
+            if (_rigidBody == null)
+            {
+                _rigidBody = this.GetComponent<Rigidbody>();
+            }
+            return _rigidBody;
+        }
+    }
+}
