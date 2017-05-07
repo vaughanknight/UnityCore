85c7c3bb8d6362d5a8adbd833095d4f3f58e8e06
Vaughan Knight <vaughan@vaughanknight.com>
2017-05-07 12:47:46 +1000

    Meta files
    Meta files back in from .gitignore as it will cause issues if I add
    scenes and prefabs etc.

---
+++ UnityCoreProject/Assets/Scripts/Core/AsyncAction.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public abstract class IAsyncUnityTask<T>
+{
+    private T _result { get; set; }
+
+    public bool Complete
+    {
+        get
+        {
+            return _complete;
+        }
+
+        set
+        {
+            _complete = value;
+        }
+    }
+
+    private bool _complete = false;
+    
+    public delegate void AyncUnityTaskCompleteHandler(object sender, AsyncUnityTaskCompleteEventArgs result);
+    public AyncUnityTaskCompleteHandler OnAsyncUnityTaskComplete;
+    public class AsyncUnityTaskCompleteEventArgs
+    {
+        public AsyncUnityTaskCompleteEventArgs(T result)
+        {
+            Result = result;
+        }
+        public T Result { get; private set; }
+    }
+    // Initialise in the foreground so we can access variables
+    // and interact with the scene
+    public abstract void InitialiseInForeground();
+    
+    // Always a thread, outside the Unity loop
+    public abstract void ExecuteInBackground();
+
+    // Hook back into the main unity thread for completion 
+    // i.e. set values, and trigger events on the main thread
+    public abstract void CompleteInForeground();
+
+    void TriggerOnCompleteInForeground()
+    {
+        if(OnAsyncUnityTaskComplete != null)
+        {
+            var args = new AsyncUnityTaskCompleteEventArgs(_result);
+            OnAsyncUnityTaskComplete(this, args);
+        }
+    }
+
+    
+}
